6ee79a062c005851dac78d3d7c35666f7afe709f
Chatura Dilan <dilan@dilan.me>
2013-11-11 19:13:28 +0530

    changes to mam vp add

--- client/js/vpp/add.js
+++ client/js/vpp/add.js
@@ -83,7 +83,37 @@ $("#btn-add").click(function() {
 
 
 $( document ).ready(function() {
-	 var ms6 = $('#ms6').magicSuggest({
-                // will fetch data from options
-     });
+	
+     
+    
+     
+     
+     jQuery.ajax({
+				url : "/mam/api/apps",
+				type : "GET",
+				dataType : "json",		
+			}).done(function(data) {
+				for(i = 0; i < data.length; i++){
+					data[i].name = data[i].attributes.overview_name;
+				}
+				
+								
+					$('#ms6').magicSuggest({
+		    width: 590,
+		    highlight: false,
+		    data: data,
+		    renderer: function(v){
+		    return '<div>' +
+		        '<div style="float:left;"><img style="width: 50px" src="' + v.attributes.images_thumbnail + '"/></div>' +
+		        '<div style="padding-left: 85px;">' +
+		            '<div style="padding-top: 20px;font-style:bold;font-size:120%;color:#333">' + v.attributes.overview_name + '</div>' +
+		            '<div style="color: #999">' + v.attributes.overview_name + '</div>' +
+		            '</div>' +
+		        '</div><div style="clear:both;"></div>';
+		    }
+		});
+	});
+     
+     
+     
 });
