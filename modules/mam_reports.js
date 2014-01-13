2351788f2fb19207638510ccb8109d7e8d066c3f
harshan <harshan@wso2.com>
2014-01-13 12:22:04 +0530

    Implemented the function to get the installed apps given user_id.

--- modules/mam_reports.js
+++ modules/mam_reports.js
@@ -64,8 +64,62 @@ var mam_reports = (function () {
             installedApps = installedApps.slice(0,10);
             
             return installedApps;
-        }
+        },
 
+        getInstalledAppsByUser: function (user_id) {
+            var queryString;
+            var results = [], app_info, result, device;
+            var deviceInfo, devicesInfo;
+            //Create the db query based on user id provided
+            if (user_id) {
+                queryString = "SELECT n.user_id,p.type_name, d.os_version, n.device_id, n.received_data FROM notifications as n JOIN (SELECT device_id, MAX(received_date) as MaxTimeStamp FROM notifications WHERE feature_code=? AND user_id=? AND received_date != 'NULL' GROUP BY device_id) dt ON (n.device_id = dt.device_id AND n.received_date = dt.MaxTimeStamp) JOIN devices as d ON (n.device_id = d.id) JOIN platforms as p ON (p.id = d.platform_id) WHERE feature_code = ? ORDER BY n.user_id,n.device_id";
+                devicesInfo = db.query(queryString, GET_APP_FEATURE_CODE, user_id, GET_APP_FEATURE_CODE);
+            } else {
+                queryString = "SELECT n.user_id,p.type_name, d.os_version, n.device_id, n.received_data FROM notifications as n JOIN (SELECT device_id, MAX(received_date) as MaxTimeStamp FROM notifications WHERE feature_code=? AND received_date != 'NULL' GROUP BY device_id) dt ON (n.device_id = dt.device_id AND n.received_date = dt.MaxTimeStamp) JOIN devices as d ON (n.device_id = d.id) JOIN platforms as p ON (p.id = d.platform_id) WHERE feature_code = ? ORDER BY n.user_id,n.device_id";
+                devicesInfo = db.query(queryString, GET_APP_FEATURE_CODE, GET_APP_FEATURE_CODE);
+            }
+            //Get the list of apps in Store
+            var appsStore = store.getAppsFromStorePackageAndName();
+            var appList = new Object();
+            //create a dictionary using the data retrieved from the appStore for quick reference
+            for (var i = 0; i < appsStore.length; i++) {
+                appList[appsStore[i].package] = appsStore[i].name;
+            }
+
+            for (var i = 0; i < devicesInfo.length;) {
+                result = new Object();
+                result.user_id = devicesInfo[i].user_id;
+                result.devices = [];
+
+                for (var j = i; j < devicesInfo.length;) {
+                    device = {"device_id": devicesInfo[j].device_id, "os_version": devicesInfo[j].os_version, "platform": devicesInfo[j].type_name, "apps": []};
+                    deviceInfo = parse(devicesInfo[j].received_data);
+                    for (var k = 0; k < deviceInfo.length; k++) {
+                        //check whether the installed app is available on app store
+                        if (appList[deviceInfo[k].package] || appList[deviceInfo[k].Identifier]) {
+                            app_info = new Object();
+                            if (device.platform === "Android") {
+                                app_info.name = appList[deviceInfo[k].package];
+                                app_info.package = deviceInfo[k].package;
+                            } else if (device.platform === "iOS") {
+                                app_info.name = appList[deviceInfo[k].Identifier];
+                                app_info.package = deviceInfo[k].Identifier;
+                            }
+                            device.apps.push(app_info);
+                        }
+                    }
+                    result.devices.push(device);
+                    j++;
+                    i = j;
+                    //break the inner loop when it reaches the data of another user
+                    if ((j < devicesInfo.length) && (result.user_id !== devicesInfo[j].user_id)) {
+                        break;
+                    }
+                }
+                results.push(result);
+            }
+            return results;
+        }
     };
     
     /* Compares by the count and sort in descending order. */
