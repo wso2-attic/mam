c0193f22509432d0dbf5d7838fdd35865745855a
Niranjan-K <niranjan.karu@gmail.com>
2014-01-22 11:17:18 +0530

    Check if user has any devices enrolled

--- modules/user.js
+++ modules/user.js
@@ -326,6 +326,27 @@ var user = (function () {
             }
             return usersByType;
         },
+        hasDevicesenrolled: function(ctx){
+            //Check if user has any devices enrolled
+            try {
+                var tenantId = common.getTenantID();
+                if(tenantId){
+                    var devices = db.query("SELECT COUNT(*) as count FROM devices WHERE user_id = ? AND tenant_id = ?", ctx.userid, tenantId);
+                    if (devices != null && devices != undefined) {
+                        if (devices[0].count > 0) {
+                            return true;
+                        }
+                    }
+                    return false;
+                } else {
+                    log.debug("Not able to get Tenant ID from Session");
+                    return null;
+                }
+            } catch(e) {
+                log.error(e);
+                return null;
+            }
+        },
         /*end of other user manager functions*/
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
